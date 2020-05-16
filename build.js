101d2e51c72a9b1ce26ea2abf6de7c248fb31b33
Denis Seleznev <hcodes@yandex.ru>
2020-05-16 11:55:33 +0300

    Update scripts for build

---
+++ build.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const exec = require('child_process').exec;
+[
+    {
+        package: 'favorite-icon',
+        name: 'Favicon'
+    },
+    {
+        package: 'favorite-icon-badge',
+        name: 'FaviconBadge'
+    },
+    {
+        package: 'favorite-icon-emoji',
+        name: 'FaviconEmoji'
+    },
+    {
+        package: 'favorite-icon-status',
+        name: 'FaviconStatus'
+    },
+    {
+        package: 'favorite-icon-video',
+        name: 'FaviconVideo'
+    }
+].map(item => {
+    [
+        { name: 'iife', file: '' },
+        { name: 'commonjs', file: 'common.' },
+        { name: 'esm', file: 'esm.' }
+    ].forEach(format => {
+        let name = '';
+        if (format.name === 'iife') {
+            name = ` --name=${item.name}`;
+        }
+        exec(`./node_modules/.bin/rollup packages/${item.package}/src/index.ts --config rollup.config.js --file packages/${item.package}/dist/index.${format.file}js --format ${format.name} ${name}`, function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
+    });
+});
