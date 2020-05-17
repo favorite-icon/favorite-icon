1ea773ded4e9afdc737e2ec8ef20a0c48c02ca93
Denis Seleznev <hcodes@yandex.ru>
2020-05-17 14:29:22 +0300

    Update index.ts

--- packages/favorite-icon-status/src/index.ts
+++ packages/favorite-icon-status/src/index.ts
@@ -8,7 +8,7 @@ export default class FaviconStatus {
     private isStatusImagesLoaded = false;
     private isFaviconLoaded = false;
     private countStatusImages = 0;
-    
+
     private status?: favicon.StatusType;
     private options?: favicon.StatusOptions;
 
@@ -41,13 +41,13 @@ export default class FaviconStatus {
         this.draw();
     }
 
-    private draw() {    
+    private draw() {
         const size =  this.options.size;
         const canvas = document.createElement('canvas');
         canvas.width = size;
         canvas.height = size;
 
-        const context = canvas.getContext('2d');    
+        const context = canvas.getContext('2d');
         context.clearRect(0, 0, size, size);
         context.drawImage(this.favicon, 0, 0, size, size);
 
@@ -61,32 +61,32 @@ export default class FaviconStatus {
             error: this.errorImage,
             warning: this.warningImage
         }[this.status], 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, x, y, width, height);
-    
+
         Favicon.set(canvas, this.options.links);
     }
 
-    private loadStatusImages() {   
+    private loadStatusImages() {
         this.okImage = new Image();
         this.errorImage = new Image();
         this.warningImage = new Image();
-        
+
         this.okImage.onload =
         this.warningImage.onload =
         this.errorImage.onload = () => {
             this.countStatusImages++;
-        
+
             if (this.countStatusImages === MAX_STATUS_IMAGE) {
                 this.isStatusImagesLoaded = true;
                 this.set();
             }
         };
-        
+
         this.errorImage.src = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z" fill="rgb(203, 36, 49)" stroke="black" stroke-width="0.5" /></svg>';
         this.okImage.src = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z" fill="rgb(40, 167, 69)" stroke="black" stroke-width="0.5" /></svg>';
         this.warningImage.src = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><circle cx="6" cy="8" r="4" fill="rgb(255, 165, 0)" stroke="black" stroke-width="0.5" /></svg>';
     }
 
-    private loadFavicon() {   
+    private loadFavicon() {
         this.favicon = new Image();
 
         this.favicon.onload =
@@ -103,3 +103,4 @@ export default class FaviconStatus {
         Favicon.reset();
     }
 }
+
