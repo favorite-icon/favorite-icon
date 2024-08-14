c2f64858afc9a175131a27cd7801b9e30bb862e1
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-15 01:45:25 +0300

    Fix example

--- examples/src/video.ts
+++ examples/src/video.ts
@@ -4,17 +4,16 @@ import './common';
 window.addEventListener('load', () => {
     const video = document.querySelector('video');
 
-    const favVideo = new FaviconVideo({ video });
+    const favVideo = new FaviconVideo();
 
     const previewVideo = new FaviconVideo({
         links: [
             document.querySelector('#preview')
         ],
         size: 64,
-        video,
     });
 
-    favVideo.start();
-    previewVideo.start();
+    favVideo.start(video);
+    previewVideo.start(video);
     video.play();
 });
