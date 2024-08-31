e1477f2c31d396dc96185e63767e0e52821f2d0b
Denis Seleznev <hcodes@users.noreply.github.com>
2024-08-31 04:28:03 +0300

    Fix replade

--- packages/favorite-icon-timeout-worker/tools/replaceWorker.mjs
+++ packages/favorite-icon-timeout-worker/tools/replaceWorker.mjs
@@ -8,6 +8,6 @@ const worker = fs.readFileSync('./dist/worker.js', 'utf8');
     './dist/index.common.js'
 ].forEach(filename => {
     const content = fs.readFileSync(filename, 'utf8').trim();
-    const data64 = 'data:text/javascript;base64,' + btoa(content);
+    const data64 = 'data:text/javascript;base64,' + btoa(worker);
     fs.writeFileSync(filename, content.replace(/\{worker\}/, data64));
 });
